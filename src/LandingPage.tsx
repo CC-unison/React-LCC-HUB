da5fef428e42103cc6149c7c3794031e16c2eb4e
Miguel Navarro <108375737+CC-unison@users.noreply.github.com>
2023-11-05 06:10:32 -0500

    First carrousel attempt

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -16,12 +16,23 @@ import {CssBaseline, Divider, Grid} from "@mui/material";
 import SoyLCC from './SoyLCCcard';
 import NoticiasCard from './noticiasCard';
 import Galeria from './Galeria';
+import LoginDialog from "./loginDialog";
+import Carousel from "better-react-carousel"
 import Footer from "./footer";
 export default function LandingPage(){
 
     const [anuncios, setAnuncios] = React.useState<any[]>([]);
     const [soyLCC, setSoyLCC] = React.useState<any[]>([]);
+    const [galeria, setGaleria] = React.useState<any[]>([]);
     const [isScrolled, setIsScrolled] = React.useState(false);
+    const [openLogin, setOpenLogin] = React.useState(false);
+
+    const handleLoginOpen = () => {
+      setOpenLogin(true);
+    }
+  const handleLoginClose = () => {
+      setOpenLogin(false);
+  }
 
     React.useEffect(() => {
       function handleScroll() {
@@ -54,6 +65,12 @@ export default function LandingPage(){
           ...doc.data(),
         })) as any[]); // Provide a type annotation to specify the type
       })
+      const galeria = query(collection(db, "galeria"), orderBy("date", "desc"));
+      onSnapshot(galeria, (querySnapshot) => {
+        setGaleria(querySnapshot.docs.map((doc) => ({
+          ...doc.data(),
+        })) as any[]); // Provide a type annotation to specify the type
+      })
     console.log("oopsie")});
 
     return(
@@ -70,7 +87,7 @@ export default function LandingPage(){
               <Button color = "inherit" sx = {{m: 1}}>Proyectos</Button>
               <Button color = "inherit" sx = {{m: 1}}>Noticias</Button>
               <Button color = "inherit" sx = {{m: 1}}>Galería</Button>
-              <Button variant = "outlined" color="inherit" sx = {{m: 1}}>Login</Button>
+              <Button variant = "outlined" color="inherit" sx = {{m: 1}} onClick = {handleLoginOpen}>Login</Button>
             </Toolbar>
           </AppBar>
         </Box>
@@ -119,7 +136,17 @@ export default function LandingPage(){
             <h2 className = "header">Galería</h2>
       <Divider className="separator" sx={{ml:"40px"}}/>
       <Box sx = {{display: "flex", ml: "40px", mt:"20px", minWidth:"100%", justifyContent: "center"}}>
-
+        <Carousel cols={2} rows = {1} gap={10} loop>
+        {galeria.map((object) => {
+        if (object.showInPage === true) {
+          return (
+            <Carousel.Item>
+              <img width="100%" src={object.url}/>
+            </Carousel.Item>
+          );
+        }
+      })}
+        </Carousel>
       </Box>
       
     </Box>
