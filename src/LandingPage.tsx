4086313904d33ab7e597c1dd5814839efd2b568d
migueln <miguelsn243@gmail.com>
2023-12-05 17:43:26 -0700

    muchas cosas

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -5,14 +5,15 @@ import './landingPage.css'
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import {db} from "./firebase"
-import {collection, query, orderBy, onSnapshot} from "firebase/firestore";
+import { db } from "./firebase"
+import { collection, query, orderBy, onSnapshot } from "firebase/firestore";
 import LCCIcon from "./assets/logo-lcc-blanco.svg"
-import {CssBaseline, Divider, Grid} from "@mui/material";
+import { CssBaseline, Divider, Grid } from "@mui/material";
 import SoyLCC from './SoyLCCcard';
 import { useMsal } from "@azure/msal-react";
-import NoticiasCard from './noticiasCard';
-import { Navigation } from 'swiper/modules';
+import {NoticiasCard} from './noticiasCard';
+import { FreeMode, Mousewheel, Navigation, Scrollbar } from 'swiper/modules';
+import { Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -21,92 +22,93 @@ import IconButton from '@mui/material/IconButton';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
-export default function LandingPage(){
+export default function LandingPage() {
 
-    const [anuncios, setAnuncios] = React.useState<any[]>([]);
-    const [loggedIn, setLogin] = React.useState<boolean>(false);
-    const [soyLCC, setSoyLCC] = React.useState<any[]>([]);
-    const [galeria, setGaleria] = React.useState<any[]>([]);
-    const [isScrolled, setIsScrolled] = React.useState(false);
-    const [openLogin, setOpenLogin] = React.useState(false);
+  const [anuncios, setAnuncios] = React.useState<any[]>([]);
+  const [loggedIn, setLogin] = React.useState<boolean>(false);
+  const [soyLCC, setSoyLCC] = React.useState<any[]>([]);
+  const [galeria, setGaleria] = React.useState<any[]>([]);
+  const [isScrolled, setIsScrolled] = React.useState(false);
+  const [openLogin, setOpenLogin] = React.useState(false);
 
-    React.useEffect(() => {
-      function handleScroll() {
-        // Check the scroll position to determine if the user has scrolled
-        if (window.scrollY > 50) {
-          setIsScrolled(true);
-        } else {
-          setIsScrolled(false);
-        }
+  React.useEffect(() => {
+    function handleScroll() {
+      // Check the scroll position to determine if the user has scrolled
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
       }
-  
-      window.addEventListener('scroll', handleScroll);
-  
-      // Remove the scroll event listener when the component unmounts
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
+    }
 
-    React.useEffect(() => {
-      const anunciosq = query(collection(db, "anuncios"), orderBy("date", "desc"));
-      onSnapshot(anunciosq, (querySnapshot) => {
-        setAnuncios(querySnapshot.docs.map((doc) => ({
-          ...doc.data(),
-        })) as any[]); // Provide a type annotation to specify the type
-      })
-      const soylccq = query(collection(db, "videos"), orderBy("date", "desc"));
-      onSnapshot(soylccq, (querySnapshot) => {
-        setSoyLCC(querySnapshot.docs.map((doc) => ({
-          ...doc.data(),
-        })) as any[]); // Provide a type annotation to specify the type
-      })
-      const galeria = query(collection(db, "galeria"), orderBy("date", "desc"));
-      onSnapshot(galeria, (querySnapshot) => {
-        setGaleria(querySnapshot.docs.map((doc) => ({
-          ...doc.data(),
-        })) as any[]); // Provide a type annotation to specify the type
-      })});
+    window.addEventListener('scroll', handleScroll);
 
-    const { instance } = useMsal();
+    // Remove the scroll event listener when the component unmounts
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
-    const loginRequest = {
-        scopes: ["User.Read"], // Define necessary scopes
-    };
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-    const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-      setAnchorEl(event.currentTarget);
-    };
-  
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
-    const handleLogin = async () => {
-      try {
-        await instance.loginPopup(loginRequest);
-        setLogin(true);
-        console.log(loggedIn)
-      } catch (error) {
-          console.log(error);
-      }
-    };
+  React.useEffect(() => {
+    const anunciosq = query(collection(db, "anuncios"), orderBy("date", "desc"));
+    onSnapshot(anunciosq, (querySnapshot) => {
+      setAnuncios(querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+      })) as any[]); // Provide a type annotation to specify the type
+    })
+    const soylccq = query(collection(db, "videos"), orderBy("date", "desc"));
+    onSnapshot(soylccq, (querySnapshot) => {
+      setSoyLCC(querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+      })) as any[]); // Provide a type annotation to specify the type
+    })
+    const galeria = query(collection(db, "galeria"), orderBy("date", "desc"));
+    onSnapshot(galeria, (querySnapshot) => {
+      setGaleria(querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+      })) as any[]); // Provide a type annotation to specify the type
+    })
+  }, []);
 
-    return(
-        <div>
-        <Box sx={{ flexGrow: 1, color:"white"} }>
-          <AppBar position="fixed"  style={{backgroundColor: isScrolled ?  "#112e5c": "transparent", transition: 'background-color 0.3s'}} sx = {{boxShadow: "none"}}>
-            
-            <Toolbar>
-            <img src={LCCIcon} width="40" height="40" style = {{margin: 20}}/>
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1}}>
-                LCC HUB
-              </Typography>
-              <Button color = "inherit" sx = {{m: 1}}>Soy LCC</Button>
-              <Button color = "inherit" sx = {{m: 1}}>Proyectos</Button>
-              <Button color = "inherit" sx = {{m: 1}}>Noticias</Button>
-              <Button color = "inherit" sx = {{m: 1}}>Galería</Button>
-              {loggedIn ? (
-                <div>
+  const { instance } = useMsal();
+
+  const loginRequest = {
+    scopes: ["User.Read"], // Define necessary scopes
+  };
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const handleLogin = async () => {
+    try {
+      await instance.loginPopup(loginRequest);
+      setLogin(true);
+      console.log(loggedIn)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1, color: "white" }}>
+        <AppBar position="fixed" style={{ backgroundColor: isScrolled ? "#112e5c" : "transparent", transition: 'background-color 0.3s' }} sx={{ boxShadow: "none" }}>
+
+          <Toolbar>
+            <img src={LCCIcon} width="40" height="40" style={{ margin: 20 }} />
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              LCC HUB
+            </Typography>
+            <Button color="inherit" sx={{ m: 1 }}>Soy LCC</Button>
+            <Button color="inherit" sx={{ m: 1 }}>Proyectos</Button>
+            <Button color="inherit" sx={{ m: 1 }}>Noticias</Button>
+            <Button color="inherit" sx={{ m: 1 }}>Galería</Button>
+            {loggedIn ? (
+              <div>
                 <IconButton
                   size="large"
                   aria-label="account of current user"
@@ -136,84 +138,124 @@ export default function LandingPage(){
                   <MenuItem onClick={handleClose}>My account</MenuItem>
                 </Menu>
               </div>
-              ):
-              <Button variant = "outlined" color="inherit" sx = {{m: 1}} onClick = {handleLogin}>Login</Button>
-              }
-            </Toolbar>
-          </AppBar>
-        </Box>
-    <Box className="container" sx={{display: "flex",flexDirection: 'column', color:"white"}}>
-      <div style={{"textAlign": "left", "marginLeft": "70px"}}>
-        <h1>LCC-HUB</h1>  
-        <h2>Seguimiento de trayectoria <br/> académica de alumnos de LCC</h2>
-        <Button variant = "contained" sx = {{p:1, minWidth:"100%", bgcolor: "background.paper", color: "text.primary", '&:hover': {
-        backgroundColor: '#f5f5f5',}}}>Iniciar sesion</Button>
-      </div>
-    </Box>
-    <Box className = "container second" sx={{display: "flex",flexDirection: 'column'}}>
-      <h2 className = "header">Soy LCC</h2>
-      <Divider className="separator" sx={{ml:"40px"}}/>
-      <Box sx = {{display: "flex", ml: "40px", mt:"20px"}}>
-      {soyLCC.map((object) => {
-        if (object.showInPage === true) {
-          return (
-            <SoyLCC
-              foto={object.img}
-              titulo={object.titulo}
-              link={object.url}
-            />
-          );
-        }
-      })}
+            ) :
+              <Button variant="outlined" color="inherit" sx={{ m: 1 }} onClick={handleLogin}>Login</Button>
+            }
+          </Toolbar>
+        </AppBar>
       </Box>
-      <h2 className = "header">Proyectos y Artículos</h2>
-      <Divider className="separator" sx={{ml:"40px"}}/>
-      <h2 className = "header">Noticias</h2>
-      <Divider className="separator" sx={{ml:"40px"}}/>
-      <Box sx = {{display: "flex", ml: "40px", mt:"20px"}}>
-      {anuncios.map((object) => {
-        if (object.showInPage === true) {
-          return (
-            <NoticiasCard
-              foto={object.imgSrc}
-              titulo={object.titulo}
-              descripcion={object.desc}
-              link={object.link}
-            />
-          );
-        }
-      })}
+      <Box className="container" sx={{ display: "flex", flexDirection: 'column', color: "white" }}>
+        <div style={{ "textAlign": "left", "marginLeft": "70px" }}>
+          <h1>LCC-HUB</h1>
+          <h2>Seguimiento de trayectoria <br /> académica de alumnos de LCC</h2>
+          <Button variant="contained" sx={{
+            p: 1, minWidth: "100%", bgcolor: "background.paper", color: "text.primary", '&:hover': {
+              backgroundColor: '#f5f5f5',
+            }
+          }}>Iniciar sesion</Button>
+        </div>
       </Box>
-            <h2 className = "header">Galería</h2>
-      <Divider className="separator" sx={{ml:"40px"}}/>
-      <Box
-  sx={{
-    display: "flex",
-    ml: "40px",
-    mt: "20px",
-    width: "95%",
-    height: "120vh",
-    overflow: "hidden",
-    justifyContent:"center",
-    pb:5
-  }}
->
-<Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-  {galeria.map((object) => {
-        if (object.showInPage === true) {
-          return (
-            <SwiperSlide>
-              <img src={object.url}/>
-            </SwiperSlide>
-          );
-        }
-      })}
+      <Box className="container second" sx={{ display: "flex", flexDirection: 'column' }}>
+        <h2 className="header">Soy LCC</h2>
+        <Divider className="separator" sx={{ ml: "40px" }} />
+        <Box sx={{display:"flex", ml: "40px", mt: "20px" , width: "95%", height:"80vh", justifyContent:"center"}}>
+        <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        freeMode={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[FreeMode, Pagination]}
+        className="mySwiper"
+      >
+        {soyLCC.map((object)=> {
+          if (object.showInPage === true) {
+            return (
+              <SwiperSlide style={{height: 500}}>
+                <SoyLCC
+                  foto={object.img}
+                  titulo={object.titulo}
+                  link={object.url}
+                />
+              </SwiperSlide>
+            );
+          }
+        })}
       </Swiper>
-</Box>
-</Box>
-    <div className="footer">
-        <Footer/>
+          {/* {soyLCC.map((object) => {
+            if (object.showInPage === true) {
+              return (
+                <SoyLCC
+                  foto={object.img}
+                  titulo={object.titulo}
+                  link={object.url}
+                />
+              );
+            }
+          })}  */}
+        </Box>
+        <h2 className="header">Proyectos y Artículos</h2>
+        <Divider className="separator" sx={{ ml: "40px" }} />
+        <h2 className="header">Noticias</h2>
+        <Divider className="separator" sx={{ ml: "40px" }} />
+        <Box sx={{display:"flex", ml: "40px", mt: "20px" , width: "95%", height:"80vh", justifyContent:"center"}}>
+        <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        freeMode={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[FreeMode, Pagination]}
+        className="mySwiper"
+      >
+        {anuncios.map((object)=> {
+          if (object.showInPage === true) {
+            return (
+              <SwiperSlide style={{height: 500}}>
+                <NoticiasCard
+                foto={object.imgSrc}
+                titulo={object.titulo}
+                descripcion={object.desc}
+                link={object.link}
+              />
+              </SwiperSlide>
+            );
+          }
+        })}
+      </Swiper>
+        </Box>
+        <h2 className="header">Galería</h2>
+        <Divider className="separator" sx={{ ml: "40px" }} />
+        <Box
+          sx={{
+            display: "flex",
+            ml: "40px",
+            mt: "20px",
+            width: "95%",
+            height: "120vh",
+            overflow: "hidden",
+            justifyContent: "center",
+            pb: 5
+          }}
+        >
+          <Swiper loop={true} navigation={true} modules={[Navigation]} className="mySwiper">
+            {galeria.map((object) => {
+              if (object.showInPage === true) {
+                return (
+                  <SwiperSlide>
+                    <img src={object.url} />
+                  </SwiperSlide>
+                );
+              }
+            })}
+          </Swiper>
+        </Box>
+      </Box>
+      <div className="footer">
+        <Footer />
       </div>
     </div>
-    )
+  )
 }
--- src/noticiasCard.tsx
+++ src/noticiasCard.tsx
@@ -1,3 +1,5 @@
+// NoticiasCard.tsx
+
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -7,23 +9,41 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 interface MyComponentProps {
-    foto: string;
-    titulo: string;
-    descripcion: string;
-    link: string;
+  foto: string;
+  titulo: string;
+  descripcion: string;
+  link: string;
+}
+
+const truncateText = (text: string, maxLength: number): string => {
+  if (text.length <= maxLength) {
+    return text;
   }
-export default function NoticiasCard({foto, titulo, descripcion, link}: MyComponentProps) {
-    function handleClick(){
-        window.open(link, '_blank')
-    }
+  return text.slice(0, maxLength) + '...';
+};
+
+export const NoticiasCard: React.FC<MyComponentProps> = ({
+  foto,
+  titulo,
+  descripcion,
+  link,
+}: MyComponentProps) => {
+  function handleClick() {
+    window.open(link, '_blank');
+  }
+
+  const truncatedDescription = truncateText(descripcion, 100); // Adjust the maximum length as needed
+
   return (
-    <Card sx={{ minWidth: 375, minHeight:345, maxWidth:375, mr:2}}>
+    <Card sx={{ minWidth: 448, maxWidth: 800, minHeight: 500, position: 'relative' }}>
       <CardMedia
-       
         component="img"
-        alt="img"
-        height="240"
-        image={foto}
+        style={{
+          height: 240,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundImage: `url(${foto})`,
+        }}
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
@@ -33,9 +53,11 @@ export default function NoticiasCard({foto, titulo, descripcion, link}: MyCompon
           {descripcion}
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small" onClick={handleClick}>Leer mas</Button>
+      <CardActions style={{ position: 'absolute', bottom: 0 }}>
+        <Button size="small" onClick={handleClick}>
+          Leer mas
+        </Button>
       </CardActions>
     </Card>
   );
-}
+};
