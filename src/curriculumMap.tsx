6ddfce872776a6c58260ccc868f5153e0767e9d3
enriquegiottonini <enriquegiottonini@gmail.com>
2024-01-11 12:26:36 -0700

    refactor: provable better performance to iterate over integers than strings.

--- src/curriculumMap.tsx
+++ src/curriculumMap.tsx
@@ -5,36 +5,51 @@ const greenBorder: React.CSSProperties = {
   border: '2px solid green',
 };
 
-const curriculumArray: string[] = [
-  "6881-6886-9100-9440-9441-0120-0123",
-  "6884-8151-9102-9442-9443-9444-0121",
-  "6889-6895-8156-9282-9447-9448",
-  "8161-9445-9446-9449-9450-0124-Selec",
-  "9156-9454-9451-9452-9453-Selec",
-  "9458-9455-9456-9457-Esp-Intg",
-  "9459-9460-9461-Esp-Esp-Intg",
-  "Esp-Esp-Intg",
+enum Eje {
+  Selec,
+  Integ,
+  Esp,
+}
+
+const mapEje2str = new Map<number, string>([
+  [Eje.Selec, "Selectivo"],
+  [Eje.Integ,"Integrador"],
+  [Eje.Esp, "Especializante"]
+]);
+
+const semesterProgram: number[][] =  [
+  [6881,6886,9100,9440,9441,120,123],
+  [6884,8151,9102,9442,9443,9444,121],
+  [6889,6895,8156,9282,9447,9448],
+  [8161,9445,9446,9449,9450,124,Eje.Selec],
+  [9156,9454,9451,9452,9453,Eje.Selec],
+  [9458,9455,9456,9457,Eje.Esp,Eje.Integ],
+  [9459,9460,9461,Eje.Esp,Eje.Esp,Eje.Integ],
+  [Eje.Esp,Eje.Esp,Eje.Integ],
 ];
 
 interface SubjectCardProps {
-  code: string;
+  code: number;
 }
 
 const SubjectCard: React.FC<SubjectCardProps> = ({ code }) => (
     <Card style={greenBorder}>
       <CardContent style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+        <Typography>
+          Creditos
+        </Typography>
         <Typography variant="h9" align="center">
-          {code}
+          {code in Eje ? mapEje2str.get(code): code}
         </Typography>
       </CardContent>
     </Card>
 );
 
 const CurriculumMap: React.FC = () => (
-    <Grid container direction="row" justifyContent="center" spacing={2}>
-      {curriculumArray.map((semester, semesterIndex) => (
-        <Grid item key={semesterIndex} direction={"column"} justifyContent="center" xs={1.1}>
-          {semester.split('-').map((subjectCode, subjectIndex) => (
+  <Grid container direction="row" justifyContent="center" spacing={2} columns={semesterProgram.length}>
+      {semesterProgram.map((semester, semesterIndex) => (
+        <Grid item key={semesterIndex} direction={"column"} justifyContent="center">
+          {semester.map((subjectCode, subjectIndex) => (
             <SubjectCard key={subjectIndex} code={subjectCode} />
           ))}
         </Grid>
