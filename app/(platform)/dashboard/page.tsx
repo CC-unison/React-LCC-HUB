3953e109968c076ee269f69d5c8f89a31e4e246a
enriquegiottonini <enriquegiottonini@gmail.com>
2024-02-10 12:15:47 -0700

    feat: basic auth with clerk.

---
+++ app/(platform)/dashboard/page.tsx
@@ -0,0 +1,14 @@
+const DashboardPage = () => {
+    return (
+        <div>
+            <div>
+                Protected
+            </div>
+            <div>
+                Not logined
+            </div>
+        </div>
+    );
+}
+
+export default DashboardPage;
--- app/(platform)/log-in/[[...log-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-const LogInPage = () => {
-    return (
-        <div>
-            LogIn Page
-        </div>
-    );
-}
-
-export default LogInPage;
