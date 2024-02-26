334df679a1a2efe154f6d01f7a821f635108146f
enriquegiottonini <enriquegiottonini@gmail.com>
2024-02-26 10:40:17 -0700

    feat: added mui cache and theme provider as suggested by the doc in nextjs integration.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,46 +1,26 @@
-"use client"
-import type { Metadata } from "next";
-import { Toolbar, IconButton, Typography, CssBaseline } from "@mui/material";
-import { Logo } from "./(hub)/_components/logo";
+"use client";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import AuthProvider from "@/lib/authConfig";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "@/utils/theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <body className={inter.className}>
-                <Toolbar sx={{ borderBottom: 1, borderColor: 'divider', backgroundColor: "white" }}>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                        href='/'
-                    >
-                        <Logo fontSize="large" />
-                    </IconButton>
-                    <Typography
-                        component="h1"
-                        variant="h5"
-                        color="black"
-                        align="center"
-                        noWrap
-                        sx={{ flex: 1, fontWeight: "bold" }}
-                    >
-                        LCC Hub
-                    </Typography>
-                </Toolbar>
-                <AuthProvider>
-                    {children}
-                </AuthProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            <AuthProvider>{children}</AuthProvider>
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
 }
