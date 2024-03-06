d003e6669bb84f926284a9345be83ecdacde484e
enriquegiottonini <enriquegiottonini@gmail.com>
2024-03-05 20:29:39 -0700

    fix: compiling

--- app/(hub)/layout.tsx
+++ app/(hub)/layout.tsx
@@ -1,23 +1,10 @@
-import { Header } from "./_components/header";
 import { MainNavbar } from "./_components/MainNavbar";
-import { Footer } from "./_components/footer";
-import { Container, CssBaseline } from "@mui/material";
-import { Main } from "next/document";
-
-const sections = [
-  { title: "Dashboard", url: "/dashboard" },
-  { title: "Soy LCC", url: "/" },
-  { title: "Noticias", url: "/" },
-  { title: "Galeria", url: "/" },
-];
 
 const HubLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      {/* <Header title="LCC Hub" sections={sections} /> */}
       <MainNavbar />
       {children}
-      {/* <Footer /> */}
     </div>
   );
 };
