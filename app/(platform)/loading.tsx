d7c8ba49631e3d2d8930e03aca00846ee213b57a
enriquegiottonini <enriquegiottonini@gmail.com>
2024-03-13 19:25:54 -0700

    fix: prettier loading page with suspense fallback in the platform pages

--- app/(platform)/loading.tsx
+++ app/(platform)/loading.tsx
@@ -1,3 +1,29 @@
+import * as React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import { Stack, Typography } from "@mui/material";
+
 export default function Loading() {
-  return <p>Loading Data...</p>;
+  return (
+    <Box
+      height="100vh"
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Stack sx={{ alignItems: "center", justifyContent: "center" }}>
+        <CircularProgress size={100} />
+        <Typography
+          fontSize={22}
+          fontWeight="bold"
+          fontStyle="oblique"
+          sx={{ pt: 3 }}
+        >
+          loading
+        </Typography>
+      </Stack>
+    </Box>
+  );
 }
